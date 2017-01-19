e2ee55aee3e40767c91747a0ac48dcc9930dd0ad
darkenvy <darkenvy6@gmail.com>
2017-01-19 12:07:06 -0800

    layed out imagemagick functionality

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 var fs = require('fs');
+var im = require('imagemagick');
 var supportedFiles = ['jpg', 'jpeg', 'png', 'gif']
 
-var imageLocations = [];
-function readdir(dir) {
+function readdir(dir, imgLocations) {
   var current = fs.readdirSync(dir);
   for (var each in current) {
     if ( fs.statSync(dir + current[each]).isDirectory() ) {
-      readdir(dir + current[each] + '/', false);
+      readdir(dir + current[each] + '/', imgLocations);
     } else {
       var ext = /\.(\w{1,3})$/.exec(dir + current[each])
       if (ext && ext[1] && supportedFiles.indexOf(ext[1].toLowerCase()) !== -1) {
-        imageLocations.push(dir + current[each]);
+        imgLocations.push(dir + current[each]);
       }
     }
   }
-  return imageLocations;
+  return imgLocations;
 }
 
-var all = readdir(__dirname + '/in/');
-console.log(imageLocations);
+function compareSize(originalFilename, newFilename) {
+
+}
+
+function identify(filename) {
+  return new Promise(function(resolve, reject) {
+    im.identify(filename, function(err, features) {
+      if (err) reject(err);
+      resolve(features);
+    })
+  })
+}
+
+function convert(filename) {
+  var arguments = [filename];
+  identify(filename)
+  .then(function(meta) {
+    if (meta.format == 'PNG') {}
+    if (meta.format == 'JPEG') {}
+    if (meta.colorspace != 'sRGB') {}
+    if (meta.depth > 8) {}
+  })
+  .then(function() {
+    console.log(filename);
+  })
+
+}
+
+var imageLocations = readdir(__dirname + '/in/', []);
+// console.log(imageLocations);
+convert(imageLocations[0])
